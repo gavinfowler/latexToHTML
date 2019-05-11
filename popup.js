9c72f1569477e39882d0ed6053b7621f79ec84c0
Gavin Fowler <gavins.username@gmail.com>
2019-05-10 21:59:04 -0600

    Add logo on chrome extensions page

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let translator = katex;
 let css = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">'
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -11,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var latex = input.value;
     latex = latex.replace(/\/[^\/]/g, "\\\\");
 
-    katex = css + katex.renderToString(latex);
+    katex = translator.renderToString(latex);
 
     var stringKatex = katex.replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
