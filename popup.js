41eb2fd60c8648c4502396a270570988c8d6a337
Gavin Fowler <gavins.username@gmail.com>
2019-05-11 10:15:41 -0600

    Add copy to clipboard button and fix spacing

--- popup.js
+++ popup.js
@@ -1,19 +1,22 @@
 'use strict';
 
 let translator = katex;
-let css = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">'
+const css = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">'
 
 document.addEventListener('DOMContentLoaded', function () {
   var input = document.getElementById("input");
   var display = document.getElementById("display");
   var htmlCode = document.getElementById("code");
+  var copyButton = document.getElementById("copy");
   var cssObj = document.getElementById("css");
   var cssTag = css.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   cssObj.innerHTML = cssTag;
+  
   document.getElementById('renderButton').onclick = function (element) {
 
+    copyButton.disabled = false;
     var latex = input.value;
-    latex = latex.replace(/\/[^\/]/g, "\\\\");
+    latex = latex.replace(/\//g, "\\\\");
 
     katex = translator.renderToString(latex);
 
@@ -22,5 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
     htmlCode.innerHTML =  stringKatex;
 
     display.innerHTML = katex;
+  };
+
+  copyButton.onclick = function(){
+    htmlCode.select();
+    document.execCommand("copy");
   }
 })
