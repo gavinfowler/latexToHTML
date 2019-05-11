ed7749c810b1d69d2cb9b6b17d0ae3a383c2fa62
Gavin Fowler <gavins.username@gmail.com>
2019-05-10 21:48:30 -0600

    added regex to replace tags with html character entities

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let css = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">'
+
 document.addEventListener('DOMContentLoaded', function () {
   var input = document.getElementById("input");
   var display = document.getElementById("display");
@@ -9,7 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
     var latex = input.value;
     latex = latex.replace(/\/[^\/]/g, "\\\\");
 
-    htmlCode.innerHTML = latex;
-    katex.render(latex, display);
+    katex = css + katex.renderToString(latex);
+
+    var stringKatex = katex.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+
+    htmlCode.innerHTML =  stringKatex;
+
+    display.innerHTML = katex;
   }
 })
