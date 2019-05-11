acf678b3e532ca2bcfc9660a5939d3608e1850f1
Gavin Fowler <gavins.username@gmail.com>
2019-05-11 11:03:43 -0600

    add saving last equation to local storage

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
+// TODO: add html styling
+
 'use strict';
 
-let translator = katex;
+var translator = katex;
 const css = '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css" integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ" crossorigin="anonymous">'
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -9,16 +11,24 @@ document.addEventListener('DOMContentLoaded', function () {
   var htmlCode = document.getElementById("code");
   var copyButton = document.getElementById("copy");
   var cssObj = document.getElementById("css");
+  var renderButton = document.getElementById('renderButton');
+
   var cssTag = css.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   cssObj.innerHTML = cssTag;
 
-  document.getElementById('renderButton').onclick = function (element) {
+  chrome.storage.local.get('latexInput', function(data) {
+    input.value = data.latexInput;
+    renderButton.click();
+  });
+
+  renderButton.onclick = function (element) {
     try {
       // enable copy button
       copyButton.disabled = false;
 
       // get value from input and fix '\'
       var latex = input.value;
+      chrome.storage.local.set({latexInput: latex});
       latex = latex.replace(/\//g, "\\\\");
 
       // translate to katex
@@ -31,6 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
       htmlCode.innerHTML = stringKatex;
       display.innerHTML = katex;
     } catch (err) {
+      // if incorrect input
       console.log(err);
       htmlCode.innerHTML = err;
       display.innerHTML = "ERROR";
